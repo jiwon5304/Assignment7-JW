cfb0da94b13a8b56907c33483597df17c4e9c029
Jiwon <yesjiwon5304@gmail.com>
2021-11-28 16:36:19 +0900

    Fix: sqllite3 database

--- cardoc/settings.py
+++ cardoc/settings.py
@@ -14,9 +14,6 @@ from pathlib       import Path
 from my_settings   import SECRET_KEY
 from datetime      import timedelta
 
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
